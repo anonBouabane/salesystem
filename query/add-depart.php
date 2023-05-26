e66aaa2206dbb1241e415f0058f7e5c9a1368957
ctechitdev <ctech.itdev@gmail.com>
2023-05-26 12:23:08 +0700

    aiy

--- query/add-depart.php
+++ query/add-depart.php
@@ -4,19 +4,24 @@ include("../setting/checksession.php");
 include("../setting/conn.php");
 
  extract($_POST);
- 
+ $swal = $conn->query("SELECT * FROM tbl_depart where dp_name = '$depart_name'");
+if ($swal->rowcount()>0)
+{
+$res = array("res" => "exist", "depart" =>$depart_name);
 
+}
+else{
  
 	$insCourse = $conn->query("INSERT INTO tbl_depart (dp_name ) VALUES('$depart_name') ");
 	if($insCourse)
 	{
-		$res = array("res" => "success" );
+		$res = array("res" => "success","depart"=>$depart_name );
 	}
 	else
 	{
-		$res = array("res" => "failed" );
+		$res = array("res" => "failed","depart"=>$depart_name );
 	}
- 
+}
 
 
 
--- query/add-page.php
+++ query/add-page.php
@@ -4,20 +4,24 @@ include("../setting/checksession.php");
 include("../setting/conn.php");
 
  extract($_POST);
- 
+ $pagerow = $conn->query("SELECT * FROM tbl_page_title where pt_name = '$page_name'");
+if ($pagerow ->rowcount()>0)
+{
+$res = array ("res" => "exist","pt_name" =>$page_name);
 
- 
+}
+ else{
 	$insCourse = $conn->query("INSERT INTO tbl_page_title (  st_id,pt_name,ptf_name ) VALUES('$sub_title','$page_name','$pf_name') ");
 	if($insCourse)
 	{
-		$res = array("res" => "success" );
+		$res = array("res" => "success","pt_name"=>$page_name);
 	}
 	else
 	{
-		$res = array("res" => "failed" );
+		$res = array("res" => "failed","pt_name"=>$page_name);
 	}
  
-
+ }
 
 
  echo json_encode($res);
