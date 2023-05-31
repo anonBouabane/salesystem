ec6958c2378d4baba4ef711d2eec5029b03ba7ea
itdevctech <itdevctech@gmail.com>
2023-05-31 13:59:30 -0800

    hum 31/5/2023 13:58

--- page/report-branch-sales.php
+++ page/report-branch-sales.php
@@ -2,8 +2,8 @@
 include("../setting/checksession.php");
 include("../setting/conn.php");
 
-$header_name = "ລາຍງານຍອດຂາຍຂອງສາຂາ";
-$header_click = "0";
+$header_name = "ລາຍງານຍອດຂາຍສາຂາ";
+$header_click = "6";
 ?>
 
 
@@ -38,161 +38,167 @@ $header_click = "0";
 
             <div class="content-wrapper">
                 <div class="content">
-                    <form action="" method="post">
-                        <div class="row">
-                            <div class="col-xl-12">
 
-                                <div class="card card-default">
-                                    <div class="card-header align-items-center">
-                                        <h2 class=""> ຍອດການຂາຍຂອງສາຂາ </h2>
+                    <div class="row">
+                        <div class="col-xl-12">
 
-
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-lg-6">
-                                            <div class="form-group">
-                                                <label for="firstName">ຈາກວັນທີ</label>
-                                                <input type="date" class="form-control" id="date_from" name="date_from" value="<?php echo date('Y-m-d'); ?>" />
-                                            </div>
-                                        </div>
-
-                                        <div class="col-lg-6">
-                                            <div class="form-group">
-                                                <label for="firstName">ຫາວັນທີ</label>
-                                                <input type="date" class="form-control" id="date_to" name="date_to" value="<?php echo date('Y-m-d'); ?>" />
-                                            </div>
-                                        </div>
+                            <div class="card card-default">
+                                <div class="card-header align-items-center">
+                                    <h2 class=""> ຍອດຂາຍສາຂາ </h2>
 
 
-                                    </div>
-
-                                    <div class="d-flex justify-content-end mt-6">
-                                        <button type="submit" name="btn_view" class="btn btn-primary mb-2 btn-pill">ສະແດງ</button>
-                                    </div>
-                                    <div class="card-body pb-0" data-simplebar style="height: 457px;">
-
-                                        <table class="align-middle mb-0 table table-borderless  " id="tableList">
-                                            <thead>
-                                                <tr>
-                                                    <th>ຊື່ສິນຄ້າ</th>
-                                                    <th>ຍອດຂາຍ</th>
-                                                    <th>ເງີນສົດ</th>
-                                                    <th>ເງີນໂອນ</th>
-                                                    <th></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                if (isset($_POST['btn_view'])) {
-
-                                                    $date_from = $_POST['date_from'];
-                                                    $date_to = $_POST['date_to'];
-
-
-
-                                                    $syntax = "  where b.date_register between '$date_from' and '$date_to' ";
-                                                    echo "$date_from $date_to ";
-                                                } else {
-                                                    $syntax = "";
-                                                }
-                                                $total_bill_price = 0;
-                                                $stmt4 = $conn->prepare("select a.item_id,sum(item_values) as item_sale, item_name, item_total_price
-                                                from tbl_bill_sale_detail a
-                                                left join tbl_item_data b on a.item_id = b.item_id
-                                                $syntax
-                                                 
-                                                group by a.item_id
-                                                order by bsd_id desc");
-
-                                                $stmt4->execute();
-                                                $i = 1;
-                                                if ($stmt4->rowCount() > 0) {
-                                                    while ($row4 = $stmt4->fetch(PDO::FETCH_ASSOC)) {
-                                                        $item_id = $row4['item_id'];
-                                                        $item_total_price = $row4['item_total_price'];
-                                                        $item_sale = $row4['item_sale'];
-                                                        $item_name = $row4['item_name'];
-
-                                                        $total_price = $item_sale * $row4['item_total_price'];
-                                                ?>
-                                                        <tr>
-                                                            <input type="hidden" name="item_id[]" id="item_id<?php echo $x; ?>" value='<?php echo "$item_id"; ?>' class="form-control">
-                                                            <td>
-                                                                <?php
-                                                                echo mb_strimwidth("$item_name", 0, 50, "...");
-                                                                ?>
-                                                            </td>
-                                                            <td>
-                                                                <input type="hidden" name="item_price_total[]" id="item_price_total<?php echo $x; ?>" value='<?php echo "$item_total_price"; ?>' class="form-control">
-
-                                                                <?php echo number_format("$total_price", 2, ",", ".") ?>
-                                                            </td>
-
-
-
-                                                            <td>
-
-
-                                                            </td>
-
-                                                        </tr>
-                                                <?php
-                                                        $total_bill_price = $total_bill_price + $total_price;
-                                                    }
-                                                }
-                                                $conn = null;
-                                                include("../setting/conn.php");
-                                                ?>
-                                            </tbody>
-                                        </table>
-                                        </from>
-
-                                    </div>
-                                    <div class="card-body  " data-simplebar style="height: 120px;">
-
-
-
-                                        <div class="row">
-                                            <div class="form-group  col-lg-6">
-                                                <label class="text-dark font-weight-medium">ລວມຍອດເງີນ</label>
-                                                <div class="form-group">
-                                                    <input type="hidden" id="bs_total_price" name="bs_total_price" value='<?php echo ""; ?>' class="form-control" autofocus>
-
-                                                    <label class="text-dark font-weight-medium">
-                                                        <td><?php echo number_format("$total_bill_price", 0, ",", ".") ?> ກີບ</td>
-                                                    </label>
-
-                                                </div>
-                                            </div>
-
-
-                                        </div>
-
-                                    </div>
                                 </div>
 
+                                <div class="card-body">
+                                    <div class="tab-content">
+                                        <form action="" method="post">
 
+                                            <div class="row">
+
+                                                <div class="form-group  col-lg-12">
+                                                    <label class="text-dark font-weight-medium">ສາຂາ</label>
+                                                    <div class="form-group">
+                                                        <select class=" form-control font" name="br_name" id="br_name" required>
+                                                            <option value=""> ເລືອກສາຂາ </option>
+                                                            <?php
+                                                            $stmt5 = $conn->prepare(" SELECT * FROM tbl_branch ");
+                                                            $stmt5->execute();
+                                                            if ($stmt5->rowCount() > 0) {
+                                                                while ($row5 = $stmt5->fetch(PDO::FETCH_ASSOC)) {
+                                                            ?> <option value="<?php echo $row5['br_name']; ?>"> <?php echo $row5['br_name']; ?></option>
+                                                            <?php
+                                                                }
+                                                            }
+                                                            ?>
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">ຈາກວັນທີ</label>
+                                                        <input type="date" class="form-control" id="date_from" name="date_from" value="<?php echo date('Y-m-d'); ?>" />
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="firstName">ຫາວັນທີ</label>
+                                                        <input type="date" class="form-control" id="date_to" name="date_to" value="<?php echo date('Y-m-d'); ?>" />
+                                                    </div>
+                                                </div>
+
+
+                                            </div>
+
+                                            <div class="d-flex justify-content-end mt-6">
+                                                <button type="submit" name="btn_view" class="btn btn-primary mb-2 btn-pill">ສະແດງ</button>
+                                            </div>
+
+
+                                            <table id="dashboardremain" class="table table-product " style="width:100%">
+                                                <thead>
+                                                    <tr>
+                                                        <th>ຊື່ສິນຄ້າ</th>
+                                                        <th>ຍອດຂາຍ</th>
+                                                        <th>ເງີນສົດ</th>
+                                                        <th>ເງີນໂອນ</th>
+
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+
+                                                    <?php
+
+                                                    if (isset($_POST['btn_view'])) {
+
+                                                        $date_from = $_POST['date_from'];
+                                                        $date_to = $_POST['date_to'];
+                                                        $br_name = $_POST['br_name'];
+
+
+                                                        $syntax = "  where b.date_register between '$date_from' and '$date_to' and br_name like '%$br_name%'  ";
+                                                        echo "$date_from $date_to $br_name";
+                                                    } else {
+                                                        $syntax = "";
+                                                    }
+
+
+                                                    $total_bill_price = 0;
+                                                    $stmt2 = $conn->prepare("  
+                                                    select sum(item_values) as item_sale,a.item_id ,item_name,br_name,item_total_price from tbl_bill_sale_detail a                     
+                                                    left join tbl_bill_sale b on a.bs_id = b.bs_id 
+                                                    left join tbl_item_data c on a.item_id = c.item_id
+                                                    left join tbl_branch d on b.br_id = d.br_id
+                                                    $syntax
+                                                    group by a.item_id
+                                            
+                                                ");
+                                                    $stmt2->execute();
+                                                    $i = 1;
+                                                    if ($stmt2->rowCount() > 0) {
+                                                        while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+                                                            $item_id = $row2['item_id'];
+                                                            $item_name = $row2['item_name'];
+                                                            $item_total_price = $row2['item_total_price'];
+                                                            $item_sale = $row2['item_sale'];
+
+
+                                                            $total_price = $item_sale * $item_total_price;
+                                                    ?>
+
+                                                            <tr>
+                                                                <input type="hidden" name="item_id[]" id="item_id<?php echo $x; ?>" value='<?php echo "$item_id"; ?>' class="form-control">
+                                                                <td>
+                                                                    <?php
+                                                                    echo mb_strimwidth("$item_name", 0, 50, "...");
+                                                                    ?>
+                                                                </td>                                                                
+                                                                <td>
+                                                                    <input type="hidden" name="item_price_total[]" id="item_price_total<?php echo $x; ?>" value='<?php echo "$item_price_total"; ?>' class="form-control">
+
+                                                                    <?php echo number_format("$total_price", 2, ",", ".") ?>
+                                                                </td>
+                                                                <td>
+
+
+                                                                </td>
+                                                                <td>
+
+                                                                </td>
+                                                            </tr>
+                                                    <?php
+                                                        }
+                                                    }
+                                                    $conn = null;
+                                                    include("../setting/conn.php");
+                                                    ?>
+
+
+
+
+                                                </tbody>
+                                            </table>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-
-
-
-
-
 
                         </div>
 
 
+
+
+                    </div>
+
+                    <?php include "footer.php"; ?>
+
                 </div>
-
-                <?php include "footer.php"; ?>
-
             </div>
 
-        </div>
 
 
-
-        <?php include("../setting/calljs.php"); ?>
+            <?php include("../setting/calljs.php"); ?>
 
 
 </body>
